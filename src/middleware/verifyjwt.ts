ade22dd551d0f0d4734f5ffe46c1d7a9f1acbaef
CaioERP <caiorabelo10@gmail.com>
2023-09-11 20:45:24 -0300

    Update verifyjwt.ts

--- src/middleware/verifyjwt.ts
+++ src/middleware/verifyjwt.ts
@@ -1,6 +1,4 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors'
-import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 
 const dotenv = require('dotenv');
@@ -10,7 +8,6 @@ const senha = process.env.JWT_SECRET || 'senhasecreta';
 const verifyJWT = (req: Request, res: Response, next: any) => {
     const bearer = req.headers.authorization || '';
     const token = bearer.replace('Bearer', '').trim();
-    console.log({token, bearer, senha})
     jwt.verify(token, senha , (err, decoded) => {
         if (err) return res.status(401).end();
         //@ts-ignore
