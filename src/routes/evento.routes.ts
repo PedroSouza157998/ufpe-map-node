9205fb9ca86bf61c5137b1e53b01bfdae4ab8804
CaioERP <caiorabelo10@gmail.com>
2023-09-17 17:16:09 -0300

    Correção uso do middleware

--- src/routes/evento.routes.ts
+++ src/routes/evento.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { eventoController} from '../controllers/evento';
+import verifyJWT from '../middleware/verifyjwt';
 
 const eventoRouter = Router();
+eventoRouter.use(verifyJWT)
 eventoRouter.get('/', eventoController.listEventos);
 eventoRouter.post('/', eventoController.insertEvento);
 eventoRouter.get('/:id', eventoController.getEvento);
