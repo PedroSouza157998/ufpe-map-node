31cac16bcf46234c770f297c469f7a4cee4a100c
Evandro Junior <evandroojuniorr@hotmail.com>
2023-09-18 23:58:19 -0300

    alterações devido ao local

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,12 @@
 import { Application } from "express";
 import Router from 'express';
 import { userRouter } from "./user.routes";
-import { localRouter } from "./local.routes";
 import { eventoRouter } from "./evento.routes";
 
 
 export const useRoutes = (app: Application) => {
     const apiRouter = Router();
     apiRouter.use('/user', userRouter);
-    apiRouter.use('/local',localRouter);
     apiRouter.use('/evento',eventoRouter);
 
     app.use('/api/v1', apiRouter);
