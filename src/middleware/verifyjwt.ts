dc6dc94b935d96b630a38a3569bd776b8b11edbd
PedroSouza157998 <pedrosouza157998@gmail.com>
2023-09-20 01:02:08 -0300

    remove middleware

--- src/middleware/verifyjwt.ts
+++ src/middleware/verifyjwt.ts
@@ -6,12 +6,14 @@ dotenv.config();
 const senha = process.env.JWT_SECRET || 'senhasecreta';
 
 const verifyJWT = (req: Request, res: Response, next: any) => {
-    const bearer = req.headers.authorization || '';
-    const token = bearer.replace('Bearer', '').trim();
-    jwt.verify(token, senha , (err, decoded) => {
-        if (err) return res.send("Error: Invalid Token").status(401);
-        //@ts-ignore
-        req.id = decoded.id;
+    // const bearer = req.headers.authorization || '';
+    // const token = bearer.replace('Bearer', '').trim();
+    // jwt.verify(token, senha , (err, decoded) => {
+    //     if (err) return res.send("Error: Invalid Token").status(401);
+    //     //@ts-ignore
+    //     req.id = decoded.id;
         next();
-})}
+// }
+// )
+}
 export default verifyJWT;
